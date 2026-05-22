28fcaf99e7e7b99049e99e3f4df88d3fc17711a0
YevhenVe <eugene.veprytskyi@gmail.com>
2026-05-22 10:48:54 -0400

    google analytics added v4 fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import CookieConsent from "@/components/common/CookieConsent/CookieConsent";
 import { ThemeProvider } from "@/context/ThemeContext";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700", "800"],
@@ -41,26 +42,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PNNWC2KP');`,
-          }}
-        />
-      </head>
+      <GoogleTagManager gtmId="GTM-PNNWC2KP" />
       <body className={poppins.className}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PNNWC2KP"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <ThemeProvider>
           <Header />
           <main>{children}</main>
