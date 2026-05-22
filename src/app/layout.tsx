fca931bcf7ec242ef707d512b54f4b9506773f6b
YevhenVe <eugene.veprytskyi@gmail.com>
2026-05-22 10:24:28 -0400

    google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.scss";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
@@ -42,6 +43,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PNNWC2KP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider>
           <Header />
           <main>{children}</main>
@@ -49,6 +58,15 @@ export default function RootLayout({
           <CookieConsent />
         </ThemeProvider>
       </body>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PNNWC2KP');
+        `}
+      </Script>
     </html>
   );
 }
